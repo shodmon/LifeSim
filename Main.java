5b07ca7488f0bfb7151b21f52fb263db3d6704e6
Shodmon Babakhanzoda <sbabakhanzoda@gmail.com>
2025-11-19 20:34:14 -0500

    first lines of code

---
+++ Main.java
@@ -0,0 +1,11 @@
+package LifeSim;
+
+public class Main {
+    public static int year = 2025;
+    public static int month = 11;
+    public static int day = 19;
+    
+    public static void main(String[] args){
+        
+    }
+}
---
+++ README.md.txt
@@ -0,0 +1,1 @@
+Life Simulator game in Java
